34b1f5681d16b4f88ed2da5ee63fcc24fcd3f5a5
DataDazzle24 <datadazzletecnologia@gmail.com>
2026-08-17 15:41:42 -0300

    feat: adicionar tema claro e aprimorar experiência global

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,15 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './auth/AuthProvider'
+import { ThemeProvider } from './theme/ThemeProvider'
 import './index.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <ThemeProvider><AuthProvider><App /></AuthProvider></ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
 )
